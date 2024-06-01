9071a41e3d80e6a5e1e52443ae9205462db6cd4b
tktharani <tktharani@gmail.com>
2024-06-02 00:29:34 +0530

    all api done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 
 /*images*/
 import searchIcon from "./assets/search.jpg"
+import cloudIcon from './assets/cloud-icon.jpg'
 import sunIcon from "./assets/sun.jpg"
 import clearIcon from "./assets/clear-sky.png"
 import dizzleIcon from "./assets/diazzlerain.avif"
@@ -10,9 +11,10 @@ import dizzlerainIcon from "./assets/dizzle.webp"
 import humditiyIcon from "./assets/humdity.webp"
 import snowIcon from "./assets/snow.png"
 import windIcon from "./assets/wind.avif"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import humditiy from './assets/humidity-icon.png'
 import windimage from './assets/wind-icon.png'
+import rainIcon from './assets/rain-icon.jpg'
 
 
  const Weatherdetails=({icon,temp,city,country,lat,long,humidity,wind})=>{
@@ -54,30 +56,114 @@ import windimage from './assets/wind-icon.png'
   </>
   )
  }
+ 
 function App() {
+  let api_key="c9d960bdde26034ac86b42c5cd126608"
+  const[text,setText]=useState("madurai")
   const[icon,setIcon]=useState(snowIcon);
   const[temp,setTemp]=useState(0);
-  const[city,setCity]=useState("madurai");
-  const[country,setCountry]=useState("India");
+  const[city,setCity]=useState("");
+  const[country,setCountry]=useState("");
   const[lat,setLat]=useState(0);
   const[long,setLong]=useState(0);
   const[humidity,setHumidity]=useState(0);
   const[wind,setWind]=useState(0);
+  const[cityNotFound,setCityNotFound]=useState(false);
+  const[loading,setLoading]=useState(false);
+  const[error,setError]=useState(null);
+  const weatherIconMap={
+    "01d":clearIcon,
+    "01n":clearIcon,
+    "02d":cloudIcon,
+    "02n":cloudIcon,
+    "03d":dizzleIcon,
+    "03n":dizzleIcon,
+    "04d":dizzleIcon,
+    "04n":dizzleIcon,
+    "09d":rainIcon,
+    "10d":rainIcon,
+    "10n":rainIcon,
+    "13d":snowIcon,
+    "13n":snowIcon,
+
+  }
+  
+  const search=async()=>{
+    setLoading(true);
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${text}
+    &appid=${api_key}&units=Metric`
+    try{
+      let res=await fetch(url);
+      let data=await res.json();
+      // console.log(data);
+      if(data.cod==='404'){
+        console.log("city not found")
+        setCityNotFound(true);
+        setLoading(false);
+        return;
+      }
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setTemp(Math.floor(data.main.temp));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setLat(data.coord.lat);
+      setLong(data.coord.lon);
+      const weatherIconcode=data.weather[0].icon;
+      setIcon(weatherIconMap[weatherIconcode] || clearIcon);
+      setCityNotFound(false);
+  
+
+
+    }
+    catch(error){
+      console.error("An Occured :",error.message)
+
+    }
+    finally{
+      setLoading(false);
+
+    }
+   }
+   const handlecity=(e)=>{
+    setText(e.target.value);
+
+   }
+   const handlekeydown=(e)=>{
+    if(e.key==='Enter'){
+      search();
+    }
+   }
+
+   useEffect(function (){
+    search()
+   },[])
+  
 
   return (
     <>
     <div class="container">
         <div className="input-container">
-            <input type="text" className="cityInput"  placeholder="search-city"/>
+            <input type="text" className="cityInput"  
+            placeholder="search-city" onChange={handlecity} value={text}
+            onKeyDown={handlekeydown}/>
             <div className="search-icon">
-                <img src={searchIcon} style={{width:"50px",height:"50px"}} />
+                <img src={searchIcon} 
+                style={{width:"50px",height:"50px"}} onClick={()=>search()}/>
             </div>
             
             
         </div>
-        <Weatherdetails icon={icon} temp={temp} 
+        
+
+       {loading  && <div className='loading-message'>Loading..</div>}
+        {error &&<div className='error-message'>{error}</div>}
+        {cityNotFound &&<div className='city-not-found'>city not found</div>}
+
+       { !loading && !cityNotFound &&<Weatherdetails icon={icon} temp={temp} 
         city={city} country={country} lat={lat} long={long}
-        humidity={humidity} wind={wind} />
+        humidity={humidity} wind={wind} />}
+
         <p className='copyright'>
           Designed by<span>  Tharani</span>
         </p>
